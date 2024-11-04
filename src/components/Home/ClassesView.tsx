8b89e094774488cabedb950e1e43fb616a33186f
AHpxMac <ahpx@yandex.com>
2024-11-04 21:49:10 +0800

    Add response if there's no classes

--- src/components/Home/ClassesView.tsx
+++ src/components/Home/ClassesView.tsx
@@ -4,6 +4,7 @@ import { timeSequence } from "../utils/time";
 import Class from "./Class";
 import EditClassDialog from "../Dialogs/EditClassdialog";
 import { useState } from "react";
+import Flex from "../Universal/Flex";
 
 export default function ClassesViewer({
     extraStyle,
@@ -33,70 +34,80 @@ export default function ClassesViewer({
                     onEdit(data);
                 }}
             />
-            <div
-                style={{
-                    ...extraStyle,
-                    display: "grid",
-                    gridTemplateColumns: "200px repeat(32, 1fr)",
-                    gridTemplateRows: "50px repeat(7, 1fr)",
-                    width: "800vw",
-                }}
-            >
+            {classes.length != 0 ? (
                 <div
                     style={{
-                        gridColumn: "span 1",
-                        gridRowStart: "2",
-                        gridRowEnd: "9",
-                        gridTemplateRows: "repeat(7, 1fr)",
+                        ...extraStyle,
                         display: "grid",
-                        alignItems: "center",
+                        gridTemplateColumns: "200px repeat(32, 1fr)",
+                        gridTemplateRows: "50px repeat(7, 1fr)",
+                        width: "800vw",
                     }}
                 >
-                    <Title1 align="center">Monday</Title1>
-                    <Title1 align="center">Tuesday</Title1>
-                    <Title1 align="center">Wednesday</Title1>
-                    <Title1 align="center">Thursday</Title1>
-                    <Title1 align="center">Friday</Title1>
-                    <Title1 align="center">Saturday</Title1>
-                    <Title1 align="center">Sunday</Title1>
+                    <div
+                        style={{
+                            gridColumn: "span 1",
+                            gridRowStart: "2",
+                            gridRowEnd: "9",
+                            gridTemplateRows: "repeat(7, 1fr)",
+                            display: "grid",
+                            alignItems: "center",
+                        }}
+                    >
+                        <Title1 align="center">Monday</Title1>
+                        <Title1 align="center">Tuesday</Title1>
+                        <Title1 align="center">Wednesday</Title1>
+                        <Title1 align="center">Thursday</Title1>
+                        <Title1 align="center">Friday</Title1>
+                        <Title1 align="center">Saturday</Title1>
+                        <Title1 align="center">Sunday</Title1>
+                    </div>
+                    <div
+                        style={{
+                            display: "grid",
+                            gridColumnStart: "2",
+                            gridColumnEnd: "34",
+                            gridTemplateColumns: "repeat(32, 1fr)",
+                            alignItems: "center",
+                        }}
+                    >
+                        {timeSequence.map((time) => (
+                            <Subtitle1 align="center">{time}</Subtitle1>
+                        ))}
+                    </div>
+                    <div
+                        style={{
+                            gridColumnStart: "2",
+                            gridColumnEnd: "34",
+                            gridRowStart: "2",
+                            gridRowEnd: "9",
+                            gridTemplateColumns: "repeat(32, 1fr)",
+                            gridTemplateRows: "repeat(7, 1fr)",
+                            display: "grid",
+                        }}
+                    >
+                        {classes.map((cls) => (
+                            <Class
+                                data={cls}
+                                onClick={(data) => {
+                                    setEdit({
+                                        open: true,
+                                        data: data,
+                                    });
+                                }}
+                            />
+                        ))}
+                    </div>
                 </div>
-                <div
-                    style={{
-                        display: "grid",
-                        gridColumnStart: "2",
-                        gridColumnEnd: "34",
-                        gridTemplateColumns: "repeat(32, 1fr)",
-                        alignItems: "center",
-                    }}
+            ) : (
+                <Flex
+                    justifyContent="center"
+                    alignItems="center"
+                    style={{ height: "50vh" }}
                 >
-                    {timeSequence.map((time) => (
-                        <Subtitle1 align="center">{time}</Subtitle1>
-                    ))}
-                </div>
-                <div
-                    style={{
-                        gridColumnStart: "2",
-                        gridColumnEnd: "34",
-                        gridRowStart: "2",
-                        gridRowEnd: "9",
-                        gridTemplateColumns: "repeat(32, 1fr)",
-                        gridTemplateRows: "repeat(7, 1fr)",
-                        display: "grid",
-                    }}
-                >
-                    {classes.map((cls) => (
-                        <Class
-                            data={cls}
-                            onClick={(data) => {
-                                setEdit({
-                                    open: true,
-                                    data: data,
-                                });
-                            }}
-                        />
-                    ))}
-                </div>
-            </div>
+                    <Title1>Please add a class to continue.</Title1>
+                </Flex>
+            )}
         </>
     );
 }
